2b0301be0d1af5f5581a950a59c97a5df801b2fd
Carlos Diaz-Padron <cpadron@mozilla.com>
2014-07-31 13:18:42 -0700

    Rip code apart to make MVP uploading contacts

--- apps/contacts-sync/js/app.js
+++ apps/contacts-sync/js/app.js
@@ -1,1 +1,13 @@
+window.onload = function() {
+  var request = window.navigator.mozContacts.getAll();
 
+  request.onsuccess = function() {
+    if(this.result) {
+      BackupService.enqueue(this.result.id);
+      this.continue();
+    } else {
+      console.log("Executing");
+      BackupService.backup();
+    }
+  };
+};
